30083410390a256af0898286deed692284d688e1
Dmitrii <fadeev@fadeev.pro>
2025-05-28 08:03:47 +0100

    first lazy setup

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,7 @@
+return {
+  opts = function(_, opts)
+    if (vim.g.colors_name or ""):find("catppuccin") then
+      opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+    end
+  end,
+}
